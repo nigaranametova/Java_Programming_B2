49c4d6160d4395be49544e9959904366a1671bdd
nigara <nigara.nametova@gmail.com>
2023-09-09 14:49:37 -0400

    my first commit

---
+++ src/day04_variables/Olympics.java
@@ -0,0 +1,26 @@
+package day04_variables;
+
+public class Olympics {
+
+    /*
+    create a variable for the year
+            print the country and year for the next couple events
+
+
+
+     */
+    public static void main (String [] args){
+    int year;
+        System.out.println("The olympics will be in these locations for the coming year");
+
+        year = 2022;
+        System.out.println("In this year of " + year + ", the olympic will happen in China");
+
+        year = 2026;
+        System.out.println("In this year of " + year + ", the olympic will happen in France");
+
+        year = 2030;
+        System.out.println("In this year of " + year + ", the olympic will happen in Italy");
+
+    }
+}
