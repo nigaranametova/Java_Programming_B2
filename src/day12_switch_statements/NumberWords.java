65431e88ad7028a93181ae246ed43e1529cf8bf2
nigara <nigara.nametova@gmail.com>
2023-09-23 15:04:24 -0400

    my new commit for day 12

---
+++ src/day12_switch_statements/NumberWords.java
@@ -0,0 +1,8 @@
+package day12_switch_statements;
+
+public class NumberWords {
+    public static void main( String [] args){
+        int n = 0; // number must be between 1 and 5 (inclusive)
+
+    }
+}
