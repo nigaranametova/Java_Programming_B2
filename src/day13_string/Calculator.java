c66b7703aa7bad5f809611f488558f64c1263777
nigara <nigara.nametova@gmail.com>
2023-09-27 22:07:45 -0400

    my new commit for day 12

---
+++ src/day13_string/Calculator.java
@@ -0,0 +1,62 @@
+package day13_string;
+
+import java.util.Scanner;
+
+/*
+declare and assign 2 number variables
+declare and assign a char variable for an operator
+
+create a calculator based on the operator picked
+    + : add num1 and num2
+    - : minus num1 and num2
+    * : multiply num1 and num2
+    / : divide num1 and num2
+    any other char: "invalid operator"
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number1: ");
+        int num1 = input.nextInt();
+        System.out.println("Enter a number 2: ");
+        int num2 = input.nextInt();
+        System.out.println("Pick Operator: \n\t+\n\t-\n\t*\n\t/\n\t%");
+        String operator = input.next();
+
+        int result = 0;
+        String message = "";
+        boolean runOrNotRun = true;
+
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            case "%":
+                result = num1 % num2;
+                break;
+            default:
+                message = "Invalid Operator";
+                runOrNotRun = true;
+                break;
+
+
+        }
+        // if (if default is run, do not run this if statement)
+        // (operator equals + || operator equals - || operator equals * || ........) ---> this will work as well
+        if (runOrNotRun) {
+
+            System.out.println("\nYou entered numbers which are " + num1 + " and " + num2 + "\nWith the " + operator + " operator you selected, the result is: " + result);
+        } else {
+            System.out.println(message);
+        }
+    }
+}
