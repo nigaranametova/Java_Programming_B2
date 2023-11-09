ce5d1c5f6e76bdf2ba275ce31b37511fdd9c1fbc
nigara <nigara.nametova@gmail.com>
2023-11-08 22:12:24 -0500

    nested Loop Intro

---
+++ src/day31_custom_classes/TrafficLight.java
@@ -0,0 +1,10 @@
+package day31_custom_classes;
+
+public class TrafficLight {
+String light;
+public TrafficLight (String color) {
+    light = color;
+}
+}
+
+
---
+++ src/day31_custom_classes/UseThisKeyword.java
@@ -0,0 +1,14 @@
+package day31_custom_classes;
+
+public class UseThisKeyword {
+    public static void main(String[] args) {
+
+        ThisKeyword t1 = new ThisKeyword(200);
+                System.out.println(t1.num);
+
+
+            }
+
+        }
+
+
