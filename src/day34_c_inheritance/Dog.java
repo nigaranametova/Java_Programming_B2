aeb0d849e79ab49c5260542508e04ae3ca87daf6
nigara <nigara.nametova@gmail.com>
2023-11-16 19:06:23 -0500

    nested Loop Intro

---
+++ src/day34_c_inheritance/Dog.java
@@ -0,0 +1,16 @@
+package day34_c_inheritance;
+
+
+
+
+    public class Dog extends Animal {
+
+        boolean isDomestic;
+
+        public void bark () {
+            System.out.println("Dog is barking.");
+        }
+
+
+    }
+
---
+++ src/day34_c_inheritance/animal/Lion.java
@@ -0,0 +1,13 @@
+package day34_c_inheritance.animal;
+
+import day34_c_inheritance.Animal;
+
+public class Lion
+        extends Animal {
+
+    boolean isWild;
+
+    public void roar () {
+        System.out.println("Lion is roaring");
+    }
+}
