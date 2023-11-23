3595e935c6d7a489b6503162652b47cb4b2ca09b
nigara <nigara.nametova@gmail.com>
2023-11-22 22:17:45 -0500

    nested Loop Intro

---
+++ src/day38_a_abstraction_interface/language/Spanish.java
@@ -0,0 +1,15 @@
+package day38_a_abstraction_interface.language;
+
+public class Spanish implements Language{
+    @Override
+    public void hi() {
+        System.out.println("Hola");
+
+    }
+
+    @Override
+    public void bye() {
+        System.out.println("Adios");
+
+    }
+}
